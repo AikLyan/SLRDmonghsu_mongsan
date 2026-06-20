2fe96ebb691926aa9f254b8f7a7b5df805143b3e
AikLyan <kotayza2001@gmail.com>
2026-06-20 14:36:29 +0630

    initial upload of SLRDMonghsu_Mongsan project

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("slrd-cache").then(cache=>{
-      return cache.addAll([
-        "/",
-        "/login.html",
-        "/user-home.html",
-        "/admin-home.html"
-      ]);
-    })
+const CACHE_NAME = "slrd-cache-v2";
+
+const FILES = [
+  "/",
+  "/login.html",
+  "/user-home.html",
+  "/admin-home.html",
+  "/manifest.json",
+  "/logo.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
